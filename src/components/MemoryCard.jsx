28a1429e9b5d4b197483818cf9e60604e89ec43a
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-17 20:43:55 +0100

    refactor: update MemoryCard to use dynamic emoji data from props and shuffle logic

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,18 +1,19 @@
-export default function MemoryCard({ handleClick }) {
-  const emojiArray = [
-    "🐶",
-    "🐷",
-    "🐙",
-    "🐛",
-    "🐵",
-    "🐶",
-    "🐷",
-    "🐙",
-    "🐛",
-    "🐵",
-  ];
+import { decodeEntity } from "html-entities";
+import { getRandomEmojis } from "../helper";
 
-  const emojiEl = emojiArray.map((emoji, index) => (
+/**
+ * todo:
+ * step1: Get ramdon 5 emojis in API
+ * step2:Duplicate each emojis
+ *step 3: Shuffle emojis data
+ */
+
+export default function MemoryCard({ handleClick, data }) {
+  const emojiArray1 = data.map((obj) => decodeEntity(obj.htmlCode[0]));
+  const emojiArray = emojiArray1.concat(emojiArray1);
+  const shuffledEmojiArray = getRandomEmojis(emojiArray, emojiArray.length);
+
+  const emojiEl = shuffledEmojiArray.map((emoji, index) => (
     <li key={index} className="card-item">
       <button className="btn btn--emoji" onClick={handleClick}>
         {emoji}
