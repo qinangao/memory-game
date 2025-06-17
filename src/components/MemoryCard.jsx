9c40d0a79fa51af02fe2c3ea33cd1ae17ca0d255
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-17 21:24:40 +0100

    refactor: clean up MemoryCard component and improve emoji rendering logic

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,25 +1,23 @@
 import { decodeEntity } from "html-entities";
 import { getRandomEmojis } from "../helper";
 
-/**
- * todo:
- * step1: Get ramdon 5 emojis in API
- * step2:Duplicate each emojis
- *step 3: Shuffle emojis data
- */
-
 export default function MemoryCard({ handleClick, data }) {
-  const emojiArray1 = data.map((obj) => decodeEntity(obj.htmlCode[0]));
-  const emojiArray = emojiArray1.concat(emojiArray1);
-  const shuffledEmojiArray = getRandomEmojis(emojiArray, emojiArray.length);
+  const initialEmojiArray = data.map((obj) => decodeEntity(obj.htmlCode[0]));
+  const pairedEmojiArray = initialEmojiArray.concat(initialEmojiArray); //Duplicate unique emojis
+  const shuffledEmojiArray = getRandomEmojis(
+    pairedEmojiArray,
+    pairedEmojiArray.length
+  );
 
-  const emojiEl = shuffledEmojiArray.map((emoji, index) => (
-    <li key={index} className="card-item">
-      <button className="btn btn--emoji" onClick={handleClick}>
-        {emoji}
-      </button>
-    </li>
-  ));
-
-  return <ul className="card-container">{emojiEl}</ul>;
+  return (
+    <ul className="card-container">
+      {shuffledEmojiArray.map((emoji, index) => (
+        <li key={index} className="card-item">
+          <button className="btn btn--emoji" onClick={handleClick}>
+            {emoji}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
