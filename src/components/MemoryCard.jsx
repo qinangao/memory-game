f6eca0f46344afe440ee865182f7a42840a3e6d9
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-17 23:08:19 +0100

    refactor: simplify emoji handling in MemoryCard component

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,8 +2,7 @@ import { decodeEntity } from "html-entities";
 import { getRandomEmojis } from "../helper";
 
 export default function MemoryCard({ handleClick, data }) {
-  const initialEmojiArray = data.map((obj) => decodeEntity(obj.htmlCode[0]));
-  const pairedEmojiArray = initialEmojiArray.concat(initialEmojiArray); //Duplicate unique emojis
+  const pairedEmojiArray = data.concat(data); //Duplicate unique emojis
   const shuffledEmojiArray = getRandomEmojis(
     pairedEmojiArray,
     pairedEmojiArray.length
@@ -13,8 +12,11 @@ export default function MemoryCard({ handleClick, data }) {
     <ul className="card-container">
       {shuffledEmojiArray.map((emoji, index) => (
         <li key={index} className="card-item">
-          <button className="btn btn--emoji" onClick={handleClick}>
-            {emoji}
+          <button
+            className="btn btn--emoji"
+            onClick={() => handleClick(emoji.name, index)}
+          >
+            {decodeEntity(emoji.htmlCode[0])}
           </button>
         </li>
       ))}
