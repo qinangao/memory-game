b6579bdf3d2af637916835c33ddb363406908a96
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-17 20:43:37 +0100

    refactor: implement async data fetching for game start and update MemoryCard with emoji data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import { useState } from "react";
 import Form from "./components/Form";
 import MemoryCard from "./components/MemoryCard";
+import { getRandomEmojis } from "./helper";
+
+const API_URL =
+  "https://emojihub.yurace.pro/api/all/category/animals-and-nature";
 
 export default function App() {
   const [isGameOn, setIsGameOn] = useState(false);
+  const [emojisData, setEmojisData] = useState([]);
 
-  function startGame(e) {
+  async function startGame(e) {
     e.preventDefault();
-    setIsGameOn(true);
+
+    try {
+      const res = await fetch(API_URL);
+      if (!res.ok) {
+        throw new Error("Something went wrong");
+      }
+      const data = await res.json();
+
+      const dataSample = getRandomEmojis(data, 5);
+
+      setEmojisData(dataSample);
+      setIsGameOn(true);
+    } catch (error) {
+      console.error(error.message);
+    }
   }
 
   function turnCard() {
@@ -18,7 +37,7 @@ export default function App() {
     <main>
       <h1>Memory</h1>
       {!isGameOn && <Form handleSubmit={startGame} />}
-      {isGameOn && <MemoryCard handleClick={turnCard} />}
+      {isGameOn && <MemoryCard handleClick={turnCard} data={emojisData} />}
     </main>
   );
 }
