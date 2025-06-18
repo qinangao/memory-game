5d67438c99767dfd715725d2e4ba409eae00236d
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-18 22:21:52 +0100

    refactor: remove unused Fisher-Yates shuffle implementation from getRandomEmojis function

--- src/helper.js
+++ src/helper.js
@@ -1,11 +1,5 @@
 export function getRandomEmojis(data, count) {
   const shuffled = [...data].sort(() => 0.5 - Math.random());
-  // Fisher-Yates shuffle
-  // for (let i = shuffled.length - 1; i > 0; i--) {
-  //   const j = Math.floor(Math.random() * (i + 1));
-  //   [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-  // }
-  // return shuffled.slice(0, count);
   return shuffled.slice(0, count);
 }
 
