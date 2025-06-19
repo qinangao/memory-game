f1eeed634c59d18e12ded496331606861ef2be5e
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-19 11:49:24 +0100

    feat: enhance MemoryCard component with EmojiButton and match card styling

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,18 +1,39 @@
 import { decodeEntity } from "html-entities";
+import EmojiButton from "./EmojiButton";
 
-export default function MemoryCard({ handleClick, data }) {
+export default function MemoryCard({
+  handleClick,
+  data,
+  selectedCard,
+  matchCards,
+}) {
+  // console.log(matchCards);
+
   return (
     <ul className="card-container">
-      {data.map((emoji, index) => (
-        <li key={index} className="card-item">
-          <button
-            className="btn btn--emoji"
-            onClick={() => handleClick(emoji.name, index)}
-          >
-            {decodeEntity(emoji.htmlCode[0])}
-          </button>
-        </li>
-      ))}
+      {data.map((emoji, index) => {
+        const selectedCardEntry = selectedCard.find(
+          (card) => card.index === index
+        );
+        const matchedCardEntry = matchCards.find(
+          (card) => card.index === index
+        );
+        const cardStyle = matchedCardEntry
+          ? "card-item--matched"
+          : selectedCardEntry
+          ? "card-item--selected"
+          : "";
+        return (
+          <li key={index} className={cardStyle}>
+            <EmojiButton
+              content={decodeEntity(emoji.htmlCode[0])}
+              onClick={() => handleClick(emoji.name, index)}
+              selectedCardEntry={selectedCardEntry}
+              matchedCardEntry={matchedCardEntry}
+            />
+          </li>
+        );
+      })}
     </ul>
   );
 }
