fccbd32ff2dc108d8aad3bfd167e074363c69dc0
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-21 21:34:33 +0100

    feat: enhance accessibility of AssistiveTechInfo component with ARIA attributes

--- src/components/AssistiveTechInfo.jsx
+++ src/components/AssistiveTechInfo.jsx
@@ -1,6 +1,6 @@
 function AssistiveTechInfo({ emojiData, matchedCards }) {
   return (
-    <section className="sr-only">
+    <section className="sr-only" aria-atomic="true" aria-live="polite">
       <h2>Game status</h2>
       <p>Number of matched pairs: {matchedCards.length / 2}.</p>
       <p>
