f4a566adcc0df38091cd7e8fa7523f850b6abff6
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-21 21:34:17 +0100

    feat: implement GameOver component to display victory message and restart option

---
+++ src/components/GameOver.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useRef } from "react";
+import RegularButton from "./RegularButton";
+
+function GameOver({ resetGame }) {
+  const divRef = useRef(null);
+  useEffect(() => {
+    divRef.current.focus();
+  }, []);
+  return (
+    <div className="wrapper wrapper--accent" ref={divRef} tabIndex={-1}>
+      <p className="p--large">You've matched all the memory cards!</p>
+      <RegularButton handleClick={resetGame}>Play Again</RegularButton>
+    </div>
+  );
+}
+
+export default GameOver;
