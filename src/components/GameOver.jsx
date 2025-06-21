c0852b538360a8aba65c817882e525d30d178b28
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-21 23:22:25 +0100

    feat: add celebratory emojis to GameOver message for enhanced user engagement

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -8,7 +8,7 @@ function GameOver({ resetGame }) {
   }, []);
   return (
     <div className="wrapper wrapper--accent" ref={divRef} tabIndex={-1}>
-      <p className="p--large">You've matched all the memory cards!</p>
+      <p className="p--large">You've matched all the memory cards! 🎉🥳</p>
       <RegularButton handleClick={resetGame}>Play Again</RegularButton>
     </div>
   );
