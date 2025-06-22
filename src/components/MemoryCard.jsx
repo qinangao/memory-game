f1e4f9a2f4c606cf0b2bba0e912b56dd372734c0
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-22 12:09:34 +0100

    feat: add Timer component to MemoryCard for enhanced gameplay experience

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,34 +1,38 @@
 import { useGame } from "../useGame";
 import EmojiButton from "./EmojiButton";
+import Timer from "./Timer";
 
 export default function MemoryCard() {
   const { emojisData, turnCard, matchCards, selectedCard } = useGame();
   return (
-    <ul className="card-container">
-      {emojisData.map((emoji, index) => {
-        const selectedCardEntry = selectedCard.find(
-          (card) => card.index === index
-        );
-        const matchedCardEntry = matchCards.find(
-          (card) => card.index === index
-        );
-        const cardStyle = matchedCardEntry
-          ? "card-item--matched"
-          : selectedCardEntry
-          ? "card-item--selected"
-          : "";
-        return (
-          <li key={index} className={cardStyle}>
-            <EmojiButton
-              emoji={emoji}
-              onClick={() => turnCard(emoji.name, index)}
-              selectedCardEntry={selectedCardEntry}
-              matchedCardEntry={matchedCardEntry}
-              index={index}
-            />
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <Timer />
+      <ul className="card-container">
+        {emojisData.map((emoji, index) => {
+          const selectedCardEntry = selectedCard.find(
+            (card) => card.index === index
+          );
+          const matchedCardEntry = matchCards.find(
+            (card) => card.index === index
+          );
+          const cardStyle = matchedCardEntry
+            ? "card-item--matched"
+            : selectedCardEntry
+            ? "card-item--selected"
+            : "";
+          return (
+            <li key={index} className={cardStyle}>
+              <EmojiButton
+                emoji={emoji}
+                onClick={() => turnCard(emoji.name, index)}
+                selectedCardEntry={selectedCardEntry}
+                matchedCardEntry={matchedCardEntry}
+                index={index}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
