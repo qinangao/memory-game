80cbfece48b25bc67abe0b2aa22f4977cdbd2170
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-22 12:09:10 +0100

    feat: wrap GameProvider with TimerProvider for improved context management

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { GameProvider } from "./useGame";
+import { TimerProvider } from "./useTimer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GameProvider>
-      <App />
-    </GameProvider>
+    <TimerProvider>
+      <GameProvider>
+        <App />
+      </GameProvider>
+    </TimerProvider>
   </React.StrictMode>
 );
