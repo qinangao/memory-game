376dc290bb61b602239f09101f93a5e9c690dbfc
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-22 12:09:38 +0100

    feat: import useTimer hook in App component for timer functionality

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import GameOver from "./components/GameOver";
 import Error from "./components/Error";
 import Title from "./components/Title";
 import { useGame } from "./useGame";
+import { useTimer } from "./useTimer";
 
 export default function App() {
   const { isGameOn, areAllCardMatched, isError } = useGame();
+
   return (
     <main>
       <Title />
